0227ed86fa6a04bf344df363a19b17c65e6a9c2c
ReynaldoAnalista <reynaldo.analista@gmail.com>
2024-11-06 09:46:36 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-// const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 require('dotenv').config(); // Importa e configura o dotenv
 
 app.use(bodyParser.json());
 
-// Configuração da conexão com o banco de dados MySQL usando variáveis de ambiente
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
+// Configuração do pool de conexões com o banco de dados MySQL usando variáveis de ambiente
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -26,25 +18,13 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-const connection = await pool.getConnection();
-
-
-// Conectar ao banco de dados
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  } else {
-    console.log('Conectado ao banco de dados MySQL');
-  }
-});
-
 /* GET home page */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* POST para inserir dados na tabela pesagem */
-router.post('/set_data', function (req, res, next) {
+router.post('/set_data', async function (req, res, next) {
   const { FUNCIONARIO, CLIENTE, DATA, UNIDADE, QTD_SACOS, VALOR_PESADO, TIPO, IMAGEM, IDPESAGEM } = req.body;
 
   const query = `
@@ -52,28 +32,34 @@ router.post('/set_data', function (req, res, next) {
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
 
-  connection.query(query, [FUNCIONARIO, CLIENTE, DATA, UNIDADE, QTD_SACOS, VALOR_PESADO, TIPO, IMAGEM, IDPESAGEM], (err, results) => {
-    if (err) {
-      console.error('Erro ao inserir a pesagem:', err);
-      res.status(500).send('Erro ao inserir a pesagem');
-    } else {
-      res.status(201).send('Pesagem inserida com sucesso');
-    }
-  });
+  let connection;
+  try {
+    connection = await pool.getConnection(); // Obtém uma conexão do pool
+    await connection.query(query, [FUNCIONARIO, CLIENTE, DATA, UNIDADE, QTD_SACOS, VALOR_PESADO, TIPO, IMAGEM, IDPESAGEM]);
+    res.status(201).send('Pesagem inserida com sucesso');
+  } catch (err) {
+    console.error('Erro ao inserir a pesagem:', err);
+    res.status(500).send('Erro ao inserir a pesagem');
+  } finally {
+    if (connection) connection.release(); // Libera a conexão de volta ao pool
+  }
 });
 
 /* GET para buscar dados da tabela pesagem */
-router.get('/get_data', function (req, res, next) {
+router.get('/get_data', async function (req, res, next) {
   const query = 'SELECT * FROM pesagem';
 
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Erro ao buscar dados:', err);
-      res.status(500).send('Erro ao buscar dados');
-    } else {
-      res.status(200).json(results);
-    }
-  });
+  let connection;
+  try {
+    connection = await pool.getConnection(); // Obtém uma conexão do pool
+    const [results] = await connection.query(query);
+    res.status(200).json(results);
+  } catch (err) {
+    console.error('Erro ao buscar dados:', err);
+    res.status(500).send('Erro ao buscar dados');
+  } finally {
+    if (connection) connection.release(); // Libera a conexão de volta ao pool
+  }
 });
 
 module.exports = router;
